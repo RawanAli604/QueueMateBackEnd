1f5d0b2e88a54dc48eba39a07e3e70242f1ff90a
RawanAli <rawan.ali.fardan@email.com>
2025-12-23 19:07:52 +0300

    added notification andwaitlistentry serializers

---
+++ serializers/waitListEntry.py
@@ -0,0 +1,14 @@
+
+from typing import Optional
+from pydantic import BaseModel
+
+class WaitlistEntrySchema(BaseModel):
+    user_id: int
+    venue_id: int
+    status: str = "waiting"
+    position: int
+    # will be calculated later that's why it's set to none
+    estimated_wait_time: Optional[int] = None
+
+    class Config:
+        orm_mode = True
